ce34eab9a3d2305909c8bdd3d7286de5cc612093
szopkopatrik <patrikszopko@gmail.com>
2024-02-29 14:26:07 +0100

    refactor: use Lombok in Nationality

---
+++ src/main/java/nationalize/Nationality.java
@@ -0,0 +1,20 @@
+package nationalize;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+@lombok.Data
+public class Nationality {
+
+    private long count;
+    private String name;
+
+    @SerializedName("country")
+    private List<Country> countries;
+
+    public static class Country {
+
+        private String countryId;
+        private float probability;
+    }
+}
